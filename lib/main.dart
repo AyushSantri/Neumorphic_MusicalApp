7d22e2b26b771992c78ecd83ed9c7ad8e1f31f2e
AyushSantri <ayushsantri222@gmail.com>
2022-01-23 22:56:58 +0530

    added a login page to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:musical_app/screen/auth/login.dart';
 import 'package:musical_app/screen/home.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: Login(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
     );
