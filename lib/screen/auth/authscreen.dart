86a31c9ee56c4c564ab140e59a4ce153cbd8edbc
AyushSantri <ayushsantri222@gmail.com>
2022-01-24 21:43:14 +0530

    shows message to user if there is any eeroe through flutter toast

--- lib/screen/auth/authscreen.dart
+++ lib/screen/auth/authscreen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -62,7 +63,7 @@ class _AuthScreenState extends State<AuthScreen> {
             .set({'username': username, 'email': email});
       }
     } catch (err) {
-      print(err);
+      Fluttertoast.showToast(msg: err.toString());
     }
   }
 
