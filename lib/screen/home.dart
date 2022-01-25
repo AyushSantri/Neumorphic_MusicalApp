296b654e181abbb8f566e51d371904953c677162
AyushSantri <ayushsantri222@gmail.com>
2022-01-25 22:31:01 +0530

    removed appbar code from home.dart

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -27,61 +26,11 @@ class _HomeState extends State<Home> {
         ]);
   }
 
-  bool animate = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(227, 225, 225, 1),
-      appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              AnimatedContainer(
-                duration: const Duration(seconds: 2),
-                curve: Curves.easeInSine,
-                height: 50,
-                width: 50,
-                decoration: animate
-                    ? decorationForNeumorphic()
-                    : BoxDecoration(
-                        color: const Color.fromRGBO(227, 225, 225, 0.6),
-                        borderRadius: BorderRadius.circular(25),
-                      ),
-                child: IconButton(
-                    splashRadius: 20,
-                    onPressed: () {
-                      setState(() {
-                        animate = !animate;
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content: Text("Searching not supported yet")));
-                      });
-                    },
-                    icon: const Icon(
-                      Icons.search,
-                      color: Colors.black54,
-                    )),
-              ),
-              Container(
-                height: 50,
-                width: 50,
-                decoration: decorationForNeumorphic(),
-                child: IconButton(
-                    splashRadius: 20,
-                    onPressed: () {
-                      setState(() {
-                        FirebaseAuth.instance.signOut();
-                      });
-                    },
-                    icon: const Icon(
-                      Icons.logout,
-                      color: Colors.black54,
-                    )),
-              ),
-            ],
-          )),
+      appBar: ,
     );
   }
 }
