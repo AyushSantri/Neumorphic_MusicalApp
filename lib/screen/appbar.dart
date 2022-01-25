17ed7d247993857fde716accacfcf9b31533bc79
AyushSantri <ayushsantri222@gmail.com>
2022-01-25 22:26:38 +0530

    added appbar.dart

---
+++ lib/screen/appbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class appBar extends StatefulWidget {
+  const appBar({Key? key}) : super(key: key);
+
+  @override
+  _appBarState createState() => _appBarState();
+}
+
+class _appBarState extends State<appBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
