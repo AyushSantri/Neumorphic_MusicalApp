c2a97be3e8ed1333e303bf66b464bba99575a49a
AyushSantri <ayushsantri222@gmail.com>
2022-01-26 23:27:46 +0530

    added Container inside Column and Row inside Container to add text

--- lib/screen/homebody.dart
+++ lib/screen/homebody.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 20, left: 15, right: 15),
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        children: [],
-      ),
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 25, left: 20, right: 20),
+          width: MediaQuery.of(context).size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Collections',
+                style:
+                    GoogleFonts.montserrat(fontSize: 15, color: Colors.black54),
+              ),
+              const Text(
+                '...',
+                style: TextStyle(color: Colors.black54),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
