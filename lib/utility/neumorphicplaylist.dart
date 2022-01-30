534e897bff066fe290340078237795f9f946df4c
AyushSantri <ayushsantri222@gmail.com>
2022-01-30 21:27:14 +0530

    moved Neumorphic container to neumorphicplaylist.dart and imported it in playlistcontainer.dart

---
+++ lib/utility/neumorphicplaylist.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class NeumorphicPlaylist extends StatelessWidget {
+  final String playlistName;
+
+  const NeumorphicPlaylist({Key? key, required this.playlistName})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 150,
+          width: 150,
+          margin: const EdgeInsets.only(top: 17, left: 13, right: 10),
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: const BorderRadius.all(Radius.circular(30)),
+            boxShadow: [
+              const BoxShadow(
+                  color: Colors.white,
+                  offset: Offset(-4.0, -4.0),
+                  blurRadius: 5.0,
+                  spreadRadius: 3.0),
+              BoxShadow(
+                  color: Colors.grey[400]!,
+                  offset: const Offset(4.0, 4.0),
+                  blurRadius: 5.0,
+                  spreadRadius: 3.0)
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(playlistName),
+        const SizedBox(
+          height: 6,
+        ),
+        const Text('10 songs'),
+      ],
+    );
+    ;
+  }
+}
