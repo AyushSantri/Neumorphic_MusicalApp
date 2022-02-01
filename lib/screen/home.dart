75bd4b4385e3952d53e40385bcea016efa2f597f
AyushSantri <ayushsantri222@gmail.com>
2022-02-01 22:52:52 +0530

    added the code for fetching JSON Data in home.dart

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:musical_app/screen/appbarforhome.dart';
 import 'package:musical_app/screen/homebody.dart';
+
+import '../entities/songs_data.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,6 +15,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var data = await rootBundle.loadString('files/songlist.json');
+    var decodedJSON = jsonDecode(data);
+
+    var fetchedData = decodedJSON['playlist1'];
+
+    SongData.songData = List.from(fetchedData)
+        .map<SongDetail>((item) => SongDetail.fromMap(item))
+        .toList();
+
+    print(SongData.songData.length);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
