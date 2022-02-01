cdb852213f69124fe9731784b010181c077360bf
AyushSantri <ayushsantri222@gmail.com>
2022-02-01 23:32:48 +0530

    added the items parsed from JSON in song_list.dart and styled them

--- lib/screen/song_list.dart
+++ lib/screen/song_list.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:musical_app/entities/songs_data.dart';
 
 class SongList extends StatefulWidget {
@@ -15,11 +14,45 @@ class _SongListState extends State<SongList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: Text('Songs'),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
       ),
-      body: Container(
-        child: Text(SongData.songData[0].title),
+      body: ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: SongData.songData.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              ListTile(
+                onTap: () {},
+                title: Text(
+                  SongData.songData[index].title,
+                  style: GoogleFonts.montserrat(
+                      fontSize: 15, color: Colors.black54),
+                ),
+                subtitle: Text(
+                  SongData.songData[index].author,
+                  style: GoogleFonts.montserrat(
+                      fontSize: 12, color: Colors.black54),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    CupertinoIcons.play,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              const Divider(
+                color: Colors.black54,
+                indent: 20,
+                endIndent: 20,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
