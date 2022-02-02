420733dd9f2f7deff71a7fcc9e738ae33ae27992
AyushSantri <ayushsantri222@gmail.com>
2022-02-02 20:33:29 +0530

    converted stateless class into stateful

--- lib/widgets/song_tile.dart
+++ lib/widgets/song_tile.dart
@@ -3,27 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:musical_app/entities/songs_data.dart';
 
-class SongTile extends StatelessWidget {
+class SongTile extends StatefulWidget {
   const SongTile({Key? key, required this.songDetail}) : super(key: key);
   final SongDetail songDetail;
 
+  @override
+  State<SongTile> createState() => _SongTileState();
+}
+
+class _SongTileState extends State<SongTile> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
       onTap: () {},
       title: Text(
-        songDetail.title,
+        widget.songDetail.title,
         style: GoogleFonts.montserrat(fontSize: 15, color: Colors.grey[850]),
       ),
       subtitle: Text(
-        songDetail.author,
+        widget.songDetail.author,
         style: GoogleFonts.montserrat(fontSize: 12, color: Colors.black38),
       ),
-      trailing: IconButton(
-        onPressed: () {},
-        icon: const Icon(
-          CupertinoIcons.play,
-          color: Colors.red,
+      trailing: Container(
+        child: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            CupertinoIcons.play,
+            color: Colors.red,
+          ),
         ),
       ),
     );
