345f09d6a400f69fef4b73144ddab2455b39e3db
AyushSantri <ayushsantri222@gmail.com>
2022-02-05 14:00:41 +0530

    added Icon on add your song container

--- lib/widgets/add_your_song.dart
+++ lib/widgets/add_your_song.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -27,6 +28,13 @@ class AddYourSong extends StatelessWidget {
                   blurRadius: 5.0,
                   spreadRadius: 3.0)
             ],
+          ),
+          child: Center(
+            child: Icon(
+              CupertinoIcons.add_circled,
+              color: Colors.grey[500],
+              size: 100,
+            ),
           ),
         ),
         const SizedBox(
