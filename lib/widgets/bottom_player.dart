1e0ec8bb1ed075f902b7ff591729d44b5462f8d4
AyushSantri <ayushsantri222@gmail.com>
2022-02-07 22:53:06 +0530

    removed everthing from bottom_player.dart

--- lib/widgets/bottom_player.dart
+++ lib/widgets/bottom_player.dart
@@ -5,25 +5,6 @@ class BottomPLayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: MediaQuery.of(context).size.width / 1.04,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(40),
-        boxShadow: [
-          const BoxShadow(
-            color: Colors.white,
-            offset: Offset(-4.0, -4.0),
-            blurRadius: 1.0,
-          ),
-          BoxShadow(
-            color: Colors.grey[400]!,
-            offset: const Offset(4.0, 4.0),
-            blurRadius: 2.0,
-          )
-        ],
-        color: Colors.grey[200],
-      ),
-    );
+    return Container();
   }
 }
