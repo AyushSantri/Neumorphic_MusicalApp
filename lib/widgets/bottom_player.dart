aa836fa269aa91a0fdabaa887397cfd11ae8d996
AyushSantri <ayushsantri222@gmail.com>
2022-02-07 22:32:28 +0530

    added style to bottom_player.dart

--- lib/widgets/bottom_player.dart
+++ lib/widgets/bottom_player.dart
@@ -10,7 +10,19 @@ class BottomPLayer extends StatelessWidget {
       width: MediaQuery.of(context).size.width / 1.04,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(40),
-        color: Colors.blue,
+        boxShadow: [
+          const BoxShadow(
+            color: Colors.white,
+            offset: Offset(-4.0, -4.0),
+            blurRadius: 1.0,
+          ),
+          BoxShadow(
+            color: Colors.grey[400]!,
+            offset: const Offset(4.0, 4.0),
+            blurRadius: 2.0,
+          )
+        ],
+        color: Colors.grey[200],
       ),
     );
   }
