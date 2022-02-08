65d3b08f127cfe086f88f0e3e6e0d9831606d8f0
AyushSantri <ayushsantri222@gmail.com>
2022-02-09 03:36:29 +0530

    removed a dart file

--- lib/widgets/playlistcontainer.dart
+++ lib/widgets/playlistcontainer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter/material.dart';
 import 'package:musical_app/widgets/add_your_song.dart';
-import 'package:musical_app/widgets/neumorphic_playlist_container.dart';
 
 class PlaylistContainer extends StatefulWidget {
   const PlaylistContainer({Key? key}) : super(key: key);
@@ -13,7 +12,6 @@ class PlaylistContainer extends StatefulWidget {
 class _PlaylistContainerState extends State<PlaylistContainer> {
   List<Widget> playlists = [
     const AddYourSong(),
-    const NeumorphicPlaylistContainer(playlistName: 'Hip-Hop')
   ];
 
   @override
