73187db4c6fe167ec7327ffd8543dd547ebab2d7
AyushSantri <ayushsantri222@gmail.com>
2022-02-09 04:32:14 +0530

    added font_awesome dependency and used it in social_media_handles.dart

--- lib/widgets/social_media_handles.dart
+++ lib/widgets/social_media_handles.dart
@@ -1,10 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/link.dart';
 
 class SocialMediaHandle extends StatelessWidget {
   const SocialMediaHandle({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Link(
+            uri: Uri.parse('https://www.linkedin.com/in/ayush-santri/'),
+            builder: (context, followLink) {
+              return IconButton(
+                  onPressed: followLink,
+                  icon: const Icon(FontAwesomeIcons.linkedin));
+            })
+      ],
+    );
   }
 }
