b31e388620bb26eac35edff17c8e769718260a17
AyushSantri <ayushsantri222@gmail.com>
2022-02-09 04:18:53 +0530

    added new dart file social_media_handles.dart

---
+++ lib/widgets/social_media_handles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SocialMediaHandle extends StatelessWidget {
+  const SocialMediaHandle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
