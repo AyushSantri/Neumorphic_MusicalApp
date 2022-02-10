c273a6557d030cbba02292b4fec1fe33e5261bf6
AyushSantri <ayushsantri222@gmail.com>
2022-02-10 23:22:43 +0530

    added an appbar title in upload_your_song.dart

--- lib/screen/upload_your_song.dart
+++ lib/screen/upload_your_song.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class UploadYourSong extends StatelessWidget {
   const UploadYourSong({Key? key}) : super(key: key);
@@ -7,10 +8,17 @@ class UploadYourSong extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         shadowColor: Colors.transparent,
+        centerTitle: true,
+        title: Text(
+          'Upload Your Song',
+          style: GoogleFonts.montserrat(fontSize: 22, color: Colors.black54),
+        ),
       ),
+      body: const Center(),
     );
   }
 }
