3108a43587b3dd8a16d86647c729a90a13e420e2
AyushSantri <ayushsantri222@gmail.com>
2022-02-10 23:27:49 +0530

    added form and converted class to statefull widget

--- lib/screen/upload_your_song.dart
+++ lib/screen/upload_your_song.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class UploadYourSong extends StatelessWidget {
+class UploadYourSong extends StatefulWidget {
   const UploadYourSong({Key? key}) : super(key: key);
 
+  @override
+  State<UploadYourSong> createState() => _UploadYourSongState();
+}
+
+class _UploadYourSongState extends State<UploadYourSong> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +23,31 @@ class UploadYourSong extends StatelessWidget {
           style: GoogleFonts.montserrat(fontSize: 22, color: Colors.black54),
         ),
       ),
-      body: const Center(),
+      body: Center(
+        child: Form(
+            child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 2,
+              child: TextFormField(
+                keyboardType: TextInputType.name,
+                key: const ValueKey('username'),
+                style: const TextStyle(color: Colors.grey),
+                decoration: InputDecoration(
+                    labelText: "username",
+                    labelStyle: GoogleFonts.montserrat()),
+                validator: (value) {
+                  if (value?.isEmpty == true) {
+                    return "Incorrect name";
+                  }
+                  return null;
+                },
+                onSaved: (value) {},
+              ),
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
