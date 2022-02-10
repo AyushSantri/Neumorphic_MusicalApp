046734a4804a62b5c253c940fab4df7d3e350510
AyushSantri <ayushsantri222@gmail.com>
2022-02-10 23:09:31 +0530

    added an appbar in upload_your_song.dart

--- lib/screen/upload_your_song.dart
+++ lib/screen/upload_your_song.dart
@@ -6,6 +6,11 @@ class UploadYourSong extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+    );
   }
 }
