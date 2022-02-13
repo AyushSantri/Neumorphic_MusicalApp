1706a02bdb58bee98045638c6953cda9f126a9b1
AyushSantri <ayushsantri222@gmail.com>
2022-02-14 01:12:12 +0530

    added padding in upload_your_song.dart

--- lib/screen/upload_your_song.dart
+++ lib/screen/upload_your_song.dart
@@ -64,115 +64,118 @@ class _UploadYourSongState extends State<UploadYourSong> {
           style: GoogleFonts.montserrat(fontSize: 22, color: Colors.black54),
         ),
       ),
-      body: Center(
-        child: Form(
-            key: _formkey,
-            child: Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width / 1.5,
-                  child: TextFormField(
-                    keyboardType: TextInputType.name,
-                    key: const ValueKey('username'),
-                    style: const TextStyle(color: Colors.grey),
-                    decoration: InputDecoration(
-                        labelText: "username",
-                        labelStyle: GoogleFonts.montserrat()),
-                    validator: (value) {
-                      if (value?.isEmpty == true) {
-                        return "Incorrect name";
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      name = value!;
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Container(
-                  height: 50,
-                  width: MediaQuery.of(context).size.width / 1.5,
-                  decoration: BoxDecoration(
-                      color: Colors.red[700],
-                      borderRadius: BorderRadius.circular(20)),
-                  child: TextButton(
-                    onPressed: () async {
-                      result = await FilePicker.platform
-                          .pickFiles(type: FileType.audio);
-
-                      if (result == null) return;
-
-                      setState(() {
-                        var path = result!.files.single.path;
-                        _file = File(path!);
-                      });
-                    },
-                    child: Text(
-                      'Add Song',
-                      style: GoogleFonts.montserrat(
-                          fontSize: 18,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: Center(
+          child: Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    child: TextFormField(
+                      keyboardType: TextInputType.name,
+                      key: const ValueKey('username'),
+                      style: const TextStyle(color: Colors.grey),
+                      decoration: InputDecoration(
+                          labelText: "username",
+                          labelStyle: GoogleFonts.montserrat()),
+                      validator: (value) {
+                        if (value?.isEmpty == true) {
+                          return "Incorrect name";
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        name = value!;
+                      },
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                result != null
-                    ? Text(
-                        result!.files.single.name,
-                        style: GoogleFonts.montserrat(fontSize: 13),
-                      )
-                    : Text(
-                        "Select a File",
-                        style: GoogleFonts.montserrat(fontSize: 13),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    decoration: BoxDecoration(
+                        color: Colors.red[700],
+                        borderRadius: BorderRadius.circular(20)),
+                    child: TextButton(
+                      onPressed: () async {
+                        result = await FilePicker.platform
+                            .pickFiles(type: FileType.audio);
+
+                        if (result == null) return;
+
+                        setState(() {
+                          var path = result!.files.single.path;
+                          _file = File(path!);
+                        });
+                      },
+                      child: Text(
+                        'Add Song',
+                        style: GoogleFonts.montserrat(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
                       ),
-                const SizedBox(
-                  height: 15,
-                ),
-                CircularStepProgressIndicator(
-                  totalSteps: 100,
-                  currentStep:
-                      _progress != null ? (_progress! * 100).floor() : 0,
-                  stepSize: 8,
-                  selectedColor: Colors.redAccent,
-                  unselectedColor: Colors.grey[200],
-                  padding: 0,
-                  width: 80,
-                  height: 80,
-                  selectedStepSize: 7,
-                  roundedCap: (_, __) => true,
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Container(
-                  height: 50,
-                  width: MediaQuery.of(context).size.width / 1.5,
-                  decoration: BoxDecoration(
-                      color: Colors.red[700],
-                      borderRadius: BorderRadius.circular(20)),
-                  child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        submitFile();
-                      });
-                    },
-                    child: Text(
-                      'Submit Song',
-                      style: GoogleFonts.montserrat(
-                          fontSize: 18,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500),
                     ),
                   ),
-                ),
-              ],
-            )),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  result != null
+                      ? Text(
+                          result!.files.single.name,
+                          style: GoogleFonts.montserrat(fontSize: 13),
+                        )
+                      : Text(
+                          "Select a File",
+                          style: GoogleFonts.montserrat(fontSize: 13),
+                        ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  CircularStepProgressIndicator(
+                    totalSteps: 100,
+                    currentStep:
+                        _progress != null ? (_progress! * 100).floor() : 0,
+                    stepSize: 8,
+                    selectedColor: Colors.redAccent,
+                    unselectedColor: Colors.grey[200],
+                    padding: 0,
+                    width: 80,
+                    height: 80,
+                    selectedStepSize: 7,
+                    roundedCap: (_, __) => true,
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    decoration: BoxDecoration(
+                        color: Colors.red[700],
+                        borderRadius: BorderRadius.circular(20)),
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          submitFile();
+                        });
+                      },
+                      child: Text(
+                        'Submit Song',
+                        style: GoogleFonts.montserrat(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+        ),
       ),
     );
   }
