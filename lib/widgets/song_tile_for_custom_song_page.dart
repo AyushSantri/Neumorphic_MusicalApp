499bd7eb6052bb0a0a57570fb299eeca03c9ea3b
AyushSantri <ayushsantri222@gmail.com>
2022-02-23 00:08:49 +0530

    created a new dart file song_tile_for_custom_song_page.dart

---
+++ lib/widgets/song_tile_for_custom_song_page.dart
@@ -0,0 +1,26 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+class SongTileForCustomSongPage extends StatefulWidget {
+  const SongTileForCustomSongPage(
+      {Key? key,
+      required this.audioPlayer,
+      required this.name,
+      required this.url})
+      : super(key: key);
+
+  final AudioPlayer audioPlayer;
+  final String name;
+  final String url;
+
+  @override
+  State<SongTileForCustomSongPage> createState() =>
+      _SongTileForCustomSongPageState();
+}
+
+class _SongTileForCustomSongPageState extends State<SongTileForCustomSongPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
