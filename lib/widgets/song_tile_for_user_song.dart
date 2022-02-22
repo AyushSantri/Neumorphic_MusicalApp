0eccefc5b2ccc8f3c62db283f6292a96034ec91a
AyushSantri <ayushsantri222@gmail.com>
2022-02-22 23:26:33 +0530

    added a song tile for user song

---
+++ lib/widgets/song_tile_for_user_song.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SongTileForUserSong extends StatefulWidget {
+  const SongTileForUserSong({Key? key}) : super(key: key);
+
+  @override
+  State<SongTileForUserSong> createState() => _SongTileForUserSongState();
+}
+
+class _SongTileForUserSongState extends State<SongTileForUserSong> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
