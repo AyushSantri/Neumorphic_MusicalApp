dbccf57dc48f985591a49bc85bebee5dc7afed2a
AyushSantri <ayushsantri222@gmail.com>
2022-02-22 23:05:45 +0530

    created a function to get data from firebase storage

--- lib/screen/custom_song_page.dart
+++ lib/screen/custom_song_page.dart
@@ -1,8 +1,27 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomSongPage extends StatelessWidget {
   const CustomSongPage({Key? key}) : super(key: key);
+
+  Future<List<Map<String, dynamic>>> _loadSong() async {
+    List<Map<String, dynamic>> songFile = [];
+
+    final ListResult result = await FirebaseStorage.instance.ref().list();
+    final List<Reference> allFiles = result.items;
+
+    await Future.forEach<Reference>(allFiles, (file) async {
+      final String fileUrl = await file.getDownloadURL();
+      final FullMetadata fileMeta = await file.getMetadata();
+      songFile.add({
+        "url": fileUrl,
+        "uploaded_by": fileMeta.customMetadata?['name'],
+      });
+    });
+    print(songFile);
+    return songFile;
+  }
 
   @override
   Widget build(BuildContext context) {
